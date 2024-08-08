788ddf4781556ba2acf9eb6c0092dae38cffa703
Pasquale Buonocore <pasquale.buonocore@hotmail.it>
2024-08-08 18:11:17 +0200

    Add coverage

--- docker-env/build_run_tests.sh
+++ docker-env/build_run_tests.sh
@@ -11,3 +11,11 @@ make -j4
 
 # Run the tests
 ./UnitTests
+
+# Move to the Unittest.dir folder
+cd CMakeFiles/UnitTests.dir
+
+# Use gcov and lcov to compute coverage and html report
+gcov test_main.cpp.gnco
+lcov --capture --directory . --output-file gtest_coverage.info
+genhtml gtest_coverage.info --output-directory ./../../code_coverage
